7706e6a33ef9aad2112311b475c780f35d195d9a
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-07-03 12:41:08 -0300

    atualização do schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,16 @@
 PRAGMA foreign_keys= ON;
 
+CREATE TABLE administradores (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cpf CHAR(11) NOT NULL,
+    FOREIGN KEY (cpf) REFERENCES usuarios(cpf)
+);
+
+CREATE TABLE bairros(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(255) NOT NULL
+)
+
 CREATE TABLE usuarios (
     cpf CHAR(11) PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
@@ -9,13 +20,30 @@ CREATE TABLE usuarios (
 );
 
 CREATE TABLE responsaveis(
-    cpf CHAR(11) PRIMARY KEY,
-    rg VARCHAR(11) NOT NULL,
-    data_nascimento DATE NOT NULL,
-    endereco VARCHAR(100) NOT NULL,
-    bairro VARCHAR(100) NOT NULL,
-    ponto_referencia VARCHAR(255),
-    endereco_trabalho TEXT,
-    bairro_trabalho VARCHAR(255),
+    cpf CHAR(11) PRIMARY KEY, -- CPF do responsável
+    rg VARCHAR(11) NOT NULL, -- Documento de identidade
+    data_nascimento DATE NOT NULL, -- Data de nascimento do responsável
+    endereco VARCHAR(100) NOT NULL, -- Endereço de residência do responsável
+    numero INTEGER NOT NULL, -- Número da residência do responsável
+    complemento VARCHAR(100), -- Complemento do endereço da residência do responsável
+    bairro INTEGER NOT NULL, -- Bairro do endereço da residência do responsável
+    ponto_referencia VARCHAR(255), -- Ponto de referência do responsável
+    endereco_trabalho VARCHAR(100), -- Endereço de trabalho do responsável
+    numero_trabalho INTEGER, -- Número de trabalho do responsável
+    complemento_trabalho VARCHAR(100), -- Complemento do endereço de trabalho do responsável
+    bairro_trabalho VARCHAR(255), -- Bairro do endereço de trabalho do responsável
+    cidade_trabalho VARCHAR(255), -- Cidade do endereço de trabalho do responsável
+    estado_trabalho CHAR(2), -- Estado do endereço de trabalho do responsável
     FOREIGN KEY (cpf) REFERENCES usuarios(cpf)
+    FOREIGN KEY (bairro) REFERENCES bairros(id)
 );
+
+
+CREATE TABLE unidades_escolares(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    endereco VARCHAR(100) NOT NULL,
+    numero INTEGER NOT NULL,
+    complemento VARCHAR(100),
+    bairro VARCHAR(100) NOT NULL
+)
